98c4c0a4cb9190d4547902b10203431af27729db
unknown <choja@a203069.fremont.lamrc.net>
2015-07-26 00:10:32 -0700

    Coursera Programming Assignment 2 by Jea Cho

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachemat<<-matrix(NA, nrow(x), ncol(x)) ##Creates an empty matrix to cache inverse
+  message("Info: cache matrix has been successfully made") #print info
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  ## Return a matrix that is the inverse of 'x'
+if (is.na(cachemat[1,1])==TRUE){ #Check if cachematrix is empty
+  inv<-solve(x) #If empty, calcultate inverse matrix of X
+  cachemat<<-inv #Save the inverse matrix as a variable named 'cachemat'
+} 
+  cachemat #Print cachemat
+
 }
